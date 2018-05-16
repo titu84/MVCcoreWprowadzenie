d7026c8f2b6ce149993ec167f51c81288d1ba554
titu84 <krzysztof.sauermann@gamil.com>
2018-05-16 19:02:15 +0200

    Init

---
+++ Events/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Events.Models;
+
+namespace Events.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Route("{name}/{age}")]
+        //public ContentResult Index(string name, int age)
+        //{
+        //    return Content($"czesć jestem {name} mam {age} lat.");
+        //}
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Roman";
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Create()
+        {          
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            UserRepository.AddUser(user);
+            return View("Roman", user);
+        }
+    }
+}
---
+++ Events/Models/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Events.Models
+{
+    static public class UserRepository
+    {
+        private static List<User> users = new List<User>();
+        public static List<User> Users
+        {
+            get
+            {
+                return users;
+            }
+        }
+        public static void AddUser(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
